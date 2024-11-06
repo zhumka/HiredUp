fe41d8af863cfb928f9af7ea5197c6689f9ef612
Zinko17 <vovazin2@gmail.com>
2024-11-06 19:41:31 +0600

    немного обновлена вьюшка главной страницы (абсолютно не доделано)

--- work/views.py
+++ work/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from work.models import Vacancy, JobCategory
+
+
 # Create your views here.
 def home_view(request):
-    return render(request,'work/home_page.html')
+    # Получаем все категории профессий
+    job_categories = JobCategory.objects.all()
+
+    # Рекомендуемые вакансии (случайно отбираем 5 вакансий)
+    recommended_vacancies = Vacancy.objects.filter(is_active=True).order_by('?')[:5]
+
+    return render(request, 'work/home_page.html', {
+        'job_categories': job_categories,
+        'recommended_vacancies': recommended_vacancies,
+    })
