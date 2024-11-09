7125a8c6532781d3733507d5d1dd114d7818c531
Zinko17 <vovazin2@gmail.com>
2024-11-09 17:54:47 +0600

    добавил путь на страницу подробного описания вакансии

--- work/urls.py
+++ work/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
 from work.views import home_view
+from . import views
 
 urlpatterns = [
-    path('',home_view,name='home')
+    path('',home_view,name='home'),
+    path('vacancy/<int:vacancy_id>/', views.vacancy_detail_view, name = 'vacancy_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
