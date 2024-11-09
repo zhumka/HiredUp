79eea7f636b8bc410b69f3bdf607e0db5b4757b0
Zinko17 <vovazin2@gmail.com>
2024-11-09 17:56:02 +0600

    добавлена вьюшка для страницы подробного описания вакансии

--- work/views.py
+++ work/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from work.models import Vacancy, JobCategory
 
@@ -14,4 +14,20 @@ def home_view(request):
     return render(request, 'work/home_page.html', {
         'job_categories': job_categories,
         'recommended_vacancies': recommended_vacancies,
+    })
+
+def vacancy_detail_view(request, vacancy_id):
+    vacancy = get_object_or_404(Vacancy, id=vacancy_id)
+    experience = vacancy.experience_required
+    if experience == 1:
+        experience_display = f"{experience} год"
+    elif 2 <= experience % 10 <= 4 and not (11 <= experience % 100 <= 14):
+        experience_display = f"{experience} года"
+    else:
+        experience_display = f"{experience} лет"
+
+    # Выводим данные вакансии на странице
+    return render(request, 'work/vacancy_detail.html', {
+        'vacancy': vacancy,
+        'experience_display': experience_display
     })
