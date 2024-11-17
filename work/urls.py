99ae356565e02eef9c9a92afad5e2646c1e993e5
Zinko17 <vovazin2@gmail.com>
2024-11-17 16:17:22 +0600

    добавил пути для создания отклика и страницы откликов

--- work/urls.py
+++ work/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     path('',home_view,name='home'),
     path('vacancy/<int:vacancy_id>/', views.vacancy_detail_view, name = 'vacancy_detail'),
+    path('vacancy/<int:vacancy_id>/apply/', views.apply_to_vacancy, name='apply_to_vacancy'),
+    path('applications/', views.applications_view, name='applications'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
