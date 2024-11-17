468d9ab463052674af41ae7dc6360e5d7c2e7924
Zinko17 <vovazin2@gmail.com>
2024-11-17 16:18:24 +0600

    добавил вьюшки создания отклика и страницы откликов

--- work/views.py
+++ work/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
-from work.models import Vacancy, JobCategory
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+
+from hiredup.wsgi import application
+from work.models import Vacancy, JobCategory, Application
 
 
 # Create your views here.
@@ -31,3 +36,53 @@ def vacancy_detail_view(request, vacancy_id):
         'vacancy': vacancy,
         'experience_display': experience_display
     })
+
+@login_required
+def apply_to_vacancy(request, vacancy_id):
+    # Проверяем, является ли пользователь соискателем
+    if request.user.user_type.user_type != 'job_seeker':
+        messages.error(request, 'Только соискатели могут откликаться на вакансии.')
+        return redirect('home')
+
+    # Получаем вакансию
+    vacancy = get_object_or_404(Vacancy, id=vacancy_id)
+    job_seeker_profile = request.user.job_seeker_profile
+
+    # Проверяем, есть ли уже отклик на эту вакансию
+    existing_application = Application.objects.filter(vacancy=vacancy, job_seeker=job_seeker_profile).first()
+
+    if existing_application:
+        messages.info(request, 'Вы уже откликнулись на эту вакансию.')
+        return redirect('vacancy_detail', vacancy_id=vacancy.id)
+
+    # Проверяем, есть ли у соискателя резюме
+    if not job_seeker_profile.resume:
+        messages.error(request, 'У вас нет резюме. Пожалуйста, добавьте резюме в свой профиль.')
+        return redirect('edit_resume')
+
+    # Создаем отклик на вакансию
+    application = Application.objects.create(
+        vacancy=vacancy,
+        job_seeker=job_seeker_profile,
+        created_at=timezone.now()
+    )
+
+    # Сообщение после успешного отклика
+    messages.success(request, 'Вы успешно откликнулись на вакансию.')
+
+    return redirect('vacancy_detail', vacancy_id=vacancy.id)
+
+@login_required
+def applications_view(request):
+    if request.user.user_type.user_type == 'employer':
+        employer_profile = request.user.employer_profile
+        vacancies = Vacancy.objects.filter(employer=employer_profile)
+        applications = Application.objects.filter(vacancy__in=vacancies)
+        return render(request, 'work/applications_employer.html', {'applications': applications})
+    elif request.user.user_type.user_type == 'job_seeker':
+        job_seeker_profile = request.user.job_seeker_profile
+        applications = Application.objects.filter(job_seeker=job_seeker_profile)
+        return render(request, 'work/applications_job_seeker.html', {'applications': applications})
+    else:
+        return redirect('home')
+
