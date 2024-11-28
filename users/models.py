c95e3530be7c43b8d42b386a2efcdef76da2ed05
Zinko17 <vovazin2@gmail.com>
2024-11-28 14:51:31 +0600

    Изменили модели профиля соискателя и резюме

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User  # Базовая модель пользователя Django
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 from django.db import models
 from django.utils import timezone
 from work.models import Profession
@@ -45,7 +45,16 @@ class Resume(models.Model):
     education = models.CharField(max_length=20, choices=EDUCATION_CHOICES)
     summary = models.TextField(blank=True, null=True, verbose_name='Общий текст резюме')
     skills = models.TextField(blank=True, null=True, verbose_name='Навыки')
-    experience = models.TextField(blank=True, null=True, verbose_name='Опыт работы (в годах)')
+    experience = models.PositiveIntegerField(
+        verbose_name="Опыт работы (в годах)",
+        validators=[
+            MinValueValidator(0, message="Опыт работы не может быть меньше 0 лет"),
+            MaxValueValidator(50, message="Опыт работы не может быть больше 50 лет")
+        ],
+        help_text="Введите количество лет опыта (от 0 до 50)",
+        blank=True,
+        null=True
+    )
     languages = models.TextField(blank=True, null=True, verbose_name='Знание языков')
     file = models.FileField(upload_to='users/static/users/resumes', null=True, blank=True)
     created_at = models.DateTimeField('Дата создания', default=timezone.now)
@@ -57,19 +66,28 @@ class Resume(models.Model):
 class JobSeekerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='job_seeker_profile')
     resume = models.OneToOneField(Resume, on_delete=models.SET_NULL, null=True, blank=True)
-    experience = models.PositiveIntegerField(
-        verbose_name="Опыт работы (в годах)",
-        validators=[
-            MinValueValidator(0, message="Опыт работы не может быть меньше 0 лет"),
-            MaxValueValidator(50, message="Опыт работы не может быть больше 50 лет")
-        ],
-        help_text="Введите количество лет опыта (от 0 до 50)",
-        blank=True,
-        null=True
-    )
     profession = models.ForeignKey(Profession, on_delete=models.CASCADE, related_name='job_seekers', null=True, blank=True)
     created_at = models.DateTimeField('Дата создания', default=timezone.now)
     status = models.BooleanField(default=False)
+    phone_number = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+        verbose_name='Номер телефона',
+        validators=[RegexValidator(r'^\+?1?\d{9,15}$', message="Введите корректный номер телефона")]
+    )  # Новое поле для телефона
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='Дата рождения'
+    )  # Новое поле для даты рождения
+    avatar = models.ImageField(
+        upload_to='users/static/users/img',  # Папка для сохранения изображений
+        default='users/static/users/img/vindicta.jpg',  # Значение по умолчанию
+        blank=True,
+        null=True,
+        verbose_name='Аватар'
+    )  # Новое поле для аватара
 
     def __str__(self):
         return f"Job Seeker {self.user.username}"
